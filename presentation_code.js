bd41d96907907bb9563a1a49add4a1cc654be2d1
Henk Bakker <spike1292@live.nl>
2014-04-21 14:35:17 +0200

    Update source code from presentation

--- presentation_code.js
+++ presentation_code.js
@@ -9,18 +9,15 @@
     var greet = function (name) {
         return 'hello ' + name;
     };
-
     //named function expression
     //https://kangax.github.io/nfe/
     var aGreet = function greet(name) {
         return 'hello ' + name;
     };
-
     //passed as argument
     var surround = function (func, name) {
         return '(' + func(name) + ')';
     };
-
     // returned
     var makeGreet = function () {
         return function (name) {
@@ -281,9 +278,9 @@
         return names[n];
     };
     var foo = {
-        diget_name: digit_name
+        digit_name: digit_name
     };
-    document.writeln(foo.diget_name(2));
+    document.writeln(foo.digit_name(2));
     //print: three
 })();
 
@@ -304,5 +301,219 @@
 })();
 
 
+//inheritance pseudo
+(function () {
+    var Car = function (speed) {
+        this.speed = speed;
+    };
+    Car.prototype.accelerates = function (level) {
+        this.speed += level;
+    };
+    //supercar inherit from car
+    var Supercar = function (speed, power) {
+        this.speed = speed;
+        this.power = power;
+    };
+    Supercar.prototype = new Car();
+    Supercar.prototype.boost = function () {
+        this.speed += this.power;
+    };
+    var s = new Supercar(3, 100);
+    s.accelerates(5);
+    s.boost();
+    document.writeln(s.speed);
+    //print: 108
+})();
+
+//inheritance proto
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Inheritance_and_the_prototype_chain
+(function () {
+    var car = {
+        speed: 0,
+        accelerates: function (level) {
+            this.speed += level;
+        }
+    };
+    //supercar inherit from car
+    var supercar = Object.create(car);
+    supercar.boost = function () {
+        this.accelerates(100);
+    };
+    car.accelerates(2);
+    supercar.boost();
+    document.writeln(car.speed, ' ', supercar.speed, ' ', supercar.hasOwnProperty('accelerates'));
+    //print: 2 102 false
+})();
+
+//inheritance copy
+(function () {
+    var extend = function (parent, child) {
+        var i, child = child || {};
+        for (i in parent) {
+            if (parent.hasOwnProperty(i)) {
+                child[i] = parent[i];
+            }
+        }
+        return child;
+    };
+    var car = {
+        speed: 0,
+        accelerates: function (level) {
+            this.speed += level;
+        }
+    };
+    //supercar inherit(copy) from car
+    var supercar = extend(car);
+    supercar.boost = function () {
+        this.accelerates(100);
+    };
+    car.accelerates(2);
+    supercar.boost();
+    document.writeln(car.speed, ' ', supercar.speed, ' ', supercar.hasOwnProperty('accelerates'));
+    //print: 2 100 false
+})();
+
+//inheritance mixin
+(function () {
+    var mixin = function () {
+        var arg, prop, child = {};
+        for (arg = 0; arg < arguments.length; arg += 1) {
+            for (prop in arguments[arg]) {
+                if (arguments[arg].hasOwnProperty(prop)) {
+                    child[prop] = arguments[arg][prop];
+                }
+            }
+        }
+        return child;
+    };
+    var car = {
+        speed: 0,
+        accelerates: function (level) {
+            this.speed += level;
+        }
+    };
+    var person = {name: 'a'};
+    //supercar inherit(copy) from car
+    var supercar = mixin(car, person);
+    supercar.name = 'supercar';
+    supercar.accelerates(3);
+
+    document.writeln(car.speed, ' ', supercar.name, ' ', supercar.speed);
+    //print: 0 supercar 3
+})();
+
+//inheritance functional
+(function () {
+    var makeCar = function () {
+        var that = {};//step 1
+        that.speed = 0;//step 3
+        that.accelerates = function (level) {
+            that.speed += level;
+        };
+        return that; //step 4
+    };
+    var makeSuperCar = function () {
+        //supercar inherit from car
+        var that = makeCar(),//step 1
+            boostLevel = 100;// step 2
+        that.boost = function () { //step 3
+            that.accelerates(boostLevel);
+        };
+        return that; //step 4
+    };
+    var c = makeCar();
+    var s = makeSuperCar();
+    c.accelerates(3);
+    s.boost();
+    document.writeln(c.speed, ' ', s.speed);
+    //print: 3 100
+})();
+
+//callback
+(function () {
+    var point = {
+        color: 'green',
+        paint: function (area) {
+            area.writeln(this.color);
+        }
+    };
+    var doSomething = function (callback) {
+        callback(document);
+    };
+    //underscore way: http://underscorejs.org/docs/underscore.html#section-65
+    var capture = function (func, obj) {
+        return function () {
+            func.apply(obj, arguments);
+        };
+    };
+    doSomething(point.paint());
+    doSomething(capture(point.paint(), point));
+    //capture the context the ECMAscript 5 way
+    doSomething(point.paint.bind(point));
+    //print: undefined green green
+})();
+
+//iterator
+(function () {
+    var numbers = [1, 4, 9];
+    numbers.forEach(function (x) {
+        document.writeln(x);
+    });//print: 1 4 9
+    numbers.every(function (x) {
+        return x % 2 === 0;
+    });//result: false
+    numbers.some(function () {
+        return x % 2 === 0;
+    });//result: true
+    numbers.map(function (x) {
+        return x * x;
+    });//result: 1, 16, 81
+    numbers.filter(function (x) {
+        return x % 2 === 0;
+    });//result: 4
+    numbers.reduce(function (x, y) {
+        return x + y;
+    });//result: 14
+})();
+
+//iterators
+(function () {
+    var numbers = [1, 4, 9];
+    var result = numbers.map(function (i) {
+        return i * 2;
+        //result: 2 8 18
+    }).filter(function (i) {
+        return i % 2 === 0;
+        //result: 2 8 18
+    }).reduce(function (i, total) {
+        return i + total;
+        //result: 28
+    });
+    document.writeln(result);
+    //print: 28
+})();
+
+//iterator
+(function () {
+    var numbers = [1, 4, 9];
+    var square = function (arr) {
+        return arr.map(function (i) {
+            return i * 2;
+        });
+    };
+    var even = function (arr) {
+        return arr.filter(function (i) {
+            return i % 2 === 0;
+        });
+    };
+    var sum = function (arr) {
+        return arr.reduce(function (i, total) {
+            return i + total;
+        });
+    };
+    var result = sum(even(square((numbers))));
+    document.writeln(result);
+    //print: 28
+})();
 
 
